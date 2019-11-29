df099489f847a6a6f716a9d6b19d500be3a6e15b
TheKingDave <daivd@langheiter.com>
2019-11-29 19:48:13 +0100

    Implemented client

--- src/client/Client.py
+++ src/client/Client.py
@@ -2,9 +2,26 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 
 from ui import MyView
 import sys
-
+import requests
 
 class MyController(QMainWindow):
+
+    def click_check(self):
+        text = self.myForm.input.toPlainText()
+
+        try:
+            resp = requests.get("http://localhost:8080", params={"text": text})
+        except Exception as e:
+            print(e)
+            return
+
+        json = resp.json()
+        print(json)
+        out = "reliable: <b>" + str(json["reliable"]) + "</b><br>"
+        out += "language: <b>" + json["language"] + "</b><br>"
+        out += "probability <b>" + str(json["prob"]) + "</b><br>"
+        self.myForm.output.clear()
+        self.myForm.output.append(out)
 
     def __init__(self):
         """
@@ -14,6 +31,8 @@ class MyController(QMainWindow):
 
         self.myForm = MyView.Ui_MainWindow()
         self.myForm.setupUi(self)
+
+        self.myForm.check.clicked.connect(self.click_check)
 
 
 if __name__ == '__main__':
