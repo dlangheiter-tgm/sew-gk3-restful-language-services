be9441865fce5dc9b0562d8812cd54ce8197d4bf
TheKingDave <daivd@langheiter.com>
2019-11-29 19:23:38 +0100

    Finished server

--- src/server/Server.py
+++ src/server/Server.py
@@ -1,3 +1,6 @@
+import json
+import pycld2 as cld2
+
 import web
 
 urls = (
@@ -7,7 +10,21 @@ urls = (
 
 class index:
     def GET(self):
-        return "Hello, world!"
+        data = web.input()
+        if not 'text' in data.keys():
+            return json.dumps({"error": "Parameter 'text' was not set."})
+
+        reliable, _, details = cld2.detect(data.text)
+
+        print(details)
+
+        ret = {
+            "reliable": reliable,
+            "language": details[0][0],
+            "short": details[0][1],
+            "prob": details[0][2]
+        }
+        return json.dumps(ret)
 
 
 if __name__ == "__main__":
