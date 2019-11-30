1d12f0f916b25a30fde506a5fbb1a879072f447d
TheKingDave <daivd@langheiter.com>
2019-11-30 19:17:13 +0100

    Added comments to server

--- src/server/Server.py
+++ src/server/Server.py
@@ -3,13 +3,40 @@ import pycld2 as cld2
 
 import web
 
+"""Definition of the accepted urls"""
 urls = (
-    '/', 'index'
+    '/', 'Index'
 )
 
 
-class index:
+class Index:
+    """
+    Class to handle requests to the '/' path
+    """
+
     def GET(self):
+        """
+        Handles the GET requests to '/'.
+
+        The endpoint expects the GET parameter 'text' to be the text is should analyze.
+        It returns a json object in this format:
+        ```
+        {
+            reliable (boolean): reliability
+            language (string): found language full name
+            short (string): found language short name
+            prob (integer): probability in percent
+        }
+        ```
+        If there is an error it will return a json object in the format:
+        ```
+        {
+            error (string): Error message
+        }
+        ```
+
+        :return: String return (stringified json object)
+        """
         data = web.input()
         if not 'text' in data.keys():
             return json.dumps({"error": "Parameter 'text' was not set."})
@@ -28,5 +55,6 @@ class index:
 
 
 if __name__ == "__main__":
+    """Runs the web server"""
     app = web.application(urls, globals())
     app.run()
